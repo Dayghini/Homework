3752aff93fec404d60d1a2fa27c58b0e6669d439
Dayghini <dayghini@gmail.com>
2019-04-03 20:14:21 +0300

    FirstHomework

---
+++ Age/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Your age after 10 years will be {0}", age + 10);
+        }
+    }
+}
