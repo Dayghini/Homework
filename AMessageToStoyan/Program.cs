861359e3349692b3dd8dc57e43e6dd027d051b9c
Dayghini <dayghini@gmail.com>
2019-04-06 01:09:54 +0300

    To CloudNStoyan

---
+++ AMessageToStoyan/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMessageToStoyan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Im writing this because Stoyan didnt give me a lecture and i have no homework but i still have to commit something
+            string stoyan = @"  HERE IS A FAT D FOR HIM!
+                         
+              [   ]   [   [ ]    ] ]  ]     
+                [  ]  [   ]   [   ]   ]  
+                    []  [  ]   [ ] 
+                    []  []  [  ]  
+                       [  ]
+                        []
+                        []
+                       /{}\
+                     /------\   
+                    {--------}
+                     IIIIIIII
+                     IIIooIII
+                     IIIllIII
+                     IIIyyIII
+                     IIIaaIII
+                     III++III
+                     IIIssIII
+                     IIIttIII
+                     IIIooIII
+                     IIIyyIII
+                     IIIaaIII
+                     IIInnIII
+                     III!!III
+                     IIIIIIII
+                    /        \
+                   /          \
+                  /            \
+                  \            /
+                   \ ___/\____/       'A Message to Stoyan'
+";
+           Console.WriteLine(stoyan);
+        }
+    }
+}
