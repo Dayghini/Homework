74fd79cad4dc1b32399c64ea9f1847201dd88469
Dayghini <dayghini@gmail.com>
2019-04-05 18:51:56 +0300

    Programming stuff

---
+++ PointCircleRectangle/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointCircleRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            double pointCircle = Math.Sqrt(Math.Pow((x - 1), 2) + Math.Pow((y - 1), 2));
+            bool checkRectangle = ((x >= -1 && x <= 5) && (y <= 1 && y >= -1));
+            if (pointCircle <= 1.5)
+            {
+                Console.WriteLine("Inside Circle ");
+            }
+            else
+            {
+                Console.WriteLine("Outside Circle");
+            }
+
+            if (checkRectangle)
+            {
+                Console.WriteLine("Inside Rectangle");
+            }
+            else
+            {
+                Console.WriteLine("Outside Rectangle");
+            }
+        }
+    }
+}
---
+++ Trapezoids/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trapezoids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double area = 0.5 * (a + b) * c;
+            Console.WriteLine("{0:f7}", area);
+            Console.ReadLine();
+        }
+    }
+}
