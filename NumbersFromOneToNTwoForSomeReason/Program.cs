938d5ab786b424c76a5262165527b7c2fdbc4a10
Dayghini <dayghini@gmail.com>
2019-04-19 21:06:05 +0300

    i have no time for games

---
+++ NumbersFromOneToNTwoForSomeReason/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersFromOneToNTwoForSomeReason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < row; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
