d8abaff6202bee7f5fe864bb903dac62761d4ca0
Dayghini <dayghini@gmail.com>
2019-07-03 21:47:13 +0300

    JOJO

---
+++ ArrayJoJo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayJoJo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(I HAVE TO WATCH JOJO);
+        }
+    }
+}
